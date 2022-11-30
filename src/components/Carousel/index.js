e747ae6e38ea98317489b64f1f9eb17ca7e2779a
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2022-11-30 19:25:01 +0530

    carousel-added

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,29 +1,65 @@
 import React, { useState } from "react";
 import Container from '@mui/material/Container';
 import Carousel from "react-elastic-carousel";
-
+import Carousel1 from "../../assets/Carousel1.svg";
+import Carousel2 from "../../assets/Carousel2.svg";
+import Carousel3 from "../../assets/Carousel3.svg";
+import "./styles.css"
 
 const breakPoints = [
     { width: 1, itemsToShow: 1 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-    { width: 768, itemsToShow: 2 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 1 },
+    { width: 768, itemsToShow: 3 },
     { width: 1200, itemsToShow: 3 }
 ];
 
 
 const CarouselView = () => {
-    const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+    // const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
 
     return (
         <div className="carousel">
             <Container maxWidth="lg">
                 <div className="carousel-wrapper">
                     <Carousel breakPoints={breakPoints}>
-                        {items.map((item) => (
-                             <div>
-
+                        {/* {items.map((item) => ( */}
+                             <div className="carousel-item">
+                                   <div className="carousel-item-img">
+                                     <img src={Carousel1} alt="carousel-img"/>
+                                   </div>
+                                   <div className="carousel-item-details">
+                                           <span className="carousel-item-text-top">Take a quiz!</span>
+                                           <span className="carousel-item-text-bottom">Learn and earn $CKB</span>
+                                   </div>
                              </div>
-                        ))}
+                             <div className="carousel-item">
+                                   <div className="carousel-item-img">
+                                     <img src={Carousel2} alt="carousel-img"/>
+                                   </div>
+                                   <div className="carousel-item-details">
+                                           <span className="carousel-item-text-top">Portfolio 🔥</span>
+                                           <span className="carousel-item-text-bottom">Track your trades in one place, not all over the place</span>
+                                   </div>
+                             </div>
+                             <div className="carousel-item">
+                                   <div className="carousel-item-img">
+                                     <img src={Carousel3} alt="carousel-img"/>
+                                   </div>
+                                   <div className="carousel-item-details">
+                                           <span className="carousel-item-text-top">Portfolio</span>
+                                           <span className="carousel-item-text-bottom">Track your trades in one place, not all over the place</span>
+                                   </div>
+                             </div>
+                             <div className="carousel-item">
+                                   <div className="carousel-item-img">
+                                     <img src={Carousel1} alt="carousel-img"/>
+                                   </div>
+                                   <div className="carousel-item-details">
+                                           <span className="carousel-item-text-top">Take a quiz!</span>
+                                           <span className="carousel-item-text-bottom">Learn and earn $CKB</span>
+                                   </div>
+                             </div>
+                        {/* ))} */}
                     </Carousel>
                 </div>
             </Container>
